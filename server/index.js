3bebf3844f38fada1440d01ad0271af946dd4779
sathwik-kadiyala <sathwikkadiyala@gmail.com>
2023-07-03 00:00:27 +0530

    thrid commit

--- server/index.js
+++ server/index.js
@@ -30,7 +30,11 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use( express.static(path.join(__dirname, './client/build')));
+
+app.get("*", function(req,res){
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
